11b9b416537ed3da1f9bd6fc57fb6c4687ac4f80
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-03 14:29:24 +0200

    Passaggi in JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23 */
+
+// chiedi il nmome
+
+
+// chiedi il cognome
+
+
+// chiedi il colore preferito
+
+
+// unisci le tre informazioni sopra con 23 finale
+
+
+// stampa in pagina
