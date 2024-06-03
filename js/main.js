578a7205ce2f073593da9a0125a05aca403ebfe9
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-03 14:39:19 +0200

    ESERCIZIO TERMINATO

--- js/main.js
+++ js/main.js
@@ -2,15 +2,26 @@
 Infine scrivi sulla pagina nomecognomecolorepreferito23 */
 
 // chiedi il nmome
+let yourName = prompt('scrivi il tuo nome');
 
+console.log(yourName);
 
 // chiedi il cognome
+let surname = prompt('scrivi il tuo cognonome');
 
+console.log(surname);
 
 // chiedi il colore preferito
+let favouriteColor = prompt('scrivi il tuo colore preferito');
+
+console.log(favouriteColor);
 
 
 // unisci le tre informazioni sopra con 23 finale
+let result = yourName + surname + favouriteColor + 23;
+
+console.log(result);
 
 
 // stampa in pagina
+document.getElementById('password').innerHTML = result;
